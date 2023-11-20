46687061a2a76f70ff1c159232c460c497bd8923
pilarzisdm <149374220+pilarzisdm@users.noreply.github.com>
2023-11-20 08:00:04 +0000

    1

--- coba.py
+++ coba.py
@@ -1,16 +1,18 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 from bs4 import BeautifulSoup
 import time
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.chrome.options import Options
 
-chrome_options = Options()
-# Comment out the headless option for now
-# chrome_options.add_argument('--headless')
-chrome_options.add_argument('log-level=2')
+# Set up Firefox options
+firefox_options = Options()
+# firefox_options.headless = True  # Uncomment this line if you want to run in headless mode
 
-# Try running without specifying the executable_path
-driver = webdriver.Chrome(options=chrome_options)
+# Create a new instance of the Firefox driver
+driver = webdriver.Firefox(options=firefox_options)
 
 katakunci = input('Masukkan kata kunci : ')
 
@@ -25,6 +27,10 @@ def search(katakunci):
         time.sleep(5)
         driver.execute_script('window.scrollTo(0, 2500);')
         time.sleep(5)
+
+        # Wait for the page to load (adjust the timeout as needed)
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'shopee-search-item-result__items')))
+
         soup_a = BeautifulSoup(driver.page_source, 'html.parser')
         products = soup_a.find('div', class_='row shopee-search-item-result__items')
         for link in products.find_all('a'):
