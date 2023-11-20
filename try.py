9bf5ecab350263c64e8fba56e1207002c5c2e3bd
pilarzisdm <149374220+pilarzisdm@users.noreply.github.com>
2023-11-20 09:46:43 +0000

    1

--- try.py
+++ try.py
@@ -14,7 +14,7 @@ init(autoreset=True)
 base_url = "https://shopee.co.id/"
 ua = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"}
 
-class shopee_shop_detil():
+class ShopeeShopDetail():
     def detil(shopid):
         try:
             url_detil = base_url+"api/v4/shop/get_shop_detail?shopid="+str(shopid)
